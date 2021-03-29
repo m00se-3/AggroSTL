00f8f712055e2415f7a968fb19c95df946bcb37f
Samuel Bridgham <sbridgham456@gmail.com>
2021-03-29 16:13:28 -0400

    Test DyArray with std::string

--- AggroSTL/tests/arrtest.cpp
+++ AggroSTL/tests/arrtest.cpp
@@ -4,14 +4,14 @@
 
 int main()
 {
-    aggro::DyArray<float> fs;
+    aggro::DyArray<std::string> fs;
     aggro::StArray<int, 5> is = { 1, 2, 3, 4, 5 };
 
     aggro::StArray<int, 5> other = { 6, 7, 8, 9, 0 };
 
     other = is;
     
-    fs = { 2.f, 3.f, 5.f };
+    fs = { "Hello", "World" };
 
     std::cout << is << '\n';
     std::cout << other << '\n';
