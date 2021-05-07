fd5e5dd49706b6ad67455ca404409e32b1f39378
Samuel Bridgham <sbridgham456@gmail.com>
2021-05-06 21:39:57 -0400

    stretched out test to include optional operations

--- AggroSTL/tests/arrtest.cpp
+++ AggroSTL/tests/arrtest.cpp
@@ -4,7 +4,7 @@
 
 int main()
 {
-    aggro::dynarr<std::string> fs ;
+    aggro::dynarr<std::string> fs;
     aggro::array<int, 5> is = { 1, 2, 3, 4, 5 };
 
     aggro::array<int, 5> other = { 6, 7, 8, 9, 0 };
@@ -16,4 +16,25 @@ int main()
     std::cout << is << '\n';
     std::cout << other << '\n';
     std::cout << fs << '\n';
+
+    auto check = fs.at(6);
+
+    if(!check)
+        std::cout << "Index 6 does not exist for string dynarr.\n";
+
+    auto check1 = other.at(3);
+
+    if(check1)
+        std::cout << "The value at index 3 of other array is " << *check1 << ".\n";
+
+    auto comp1 = is.at(2);
+    auto comp2 = other.at(4);
+
+    if(comp1 && comp2)
+    {
+        const char* res = (comp2 == comp1) ? "are" : "are not";
+
+        std::cout << comp1.value_or(500) << " and " << comp2.value() << " " << res << " equal.\n";
+    }
+
 }
