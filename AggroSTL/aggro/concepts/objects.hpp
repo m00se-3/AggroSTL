d120cebdecf6c3a2df5e424b700fdf0d2424ca77
Samuel Brisgham <moosethree47@gmail.com>
2021-05-22 14:17:24 -0700

    <type_traits> used instead of <concepts>

--- AggroSTL/aggro/concepts/objects.hpp
+++ AggroSTL/aggro/concepts/objects.hpp
@@ -1,15 +1,18 @@
 #ifndef OBJECTLIFE_HPP
 #define OBJECTLIFE_HPP
 
-#include <concepts>
+#include <type_traits>
 
 namespace aggro
 {
+    template<typename T, typename U>
+    concept same = std::is_same_v<T, U>;
+    
     template<typename T>
     concept pointer = std::is_pointer_v<T>;
     
     template<typename T>
-    concept destructible = std::destructible<T>;
+    concept destructible = std::is_destructible_v<T>;
 
     template<typename T>
     concept default_constructible = std::is_default_constructible_v<T>;
@@ -17,12 +20,12 @@ namespace aggro
     template<typename T, typename U>
     concept fully_comparable = requires(T t, U u)
     {
-        { t == u } -> std::same_as<bool>;
-        { t != u } -> std::same_as<bool>;
-        { t < u } -> std::same_as<bool>;
-        { t > u } -> std::same_as<bool>;
-        { t <= u } -> std::same_as<bool>;
-        { t >= u } -> std::same_as<bool>;
+        { t == u } -> same<bool>;
+        { t != u } -> same<bool>;
+        { t < u } -> same<bool>;
+        { t > u } -> same<bool>;
+        { t <= u } -> same<bool>;
+        { t >= u } -> same<bool>;
     };
 
 } // namespace aggro
