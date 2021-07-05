0c1eecf756747d0aabc5f9921a0a8eeece109a7b
Samuel Bridgham <sbridgham456@gmail.com>
2021-07-05 13:39:42 -0400

    added documentation and allocate() changes

--- AggroSTL/aggro/allocators/standard.hpp
+++ AggroSTL/aggro/allocators/standard.hpp
@@ -13,26 +13,30 @@ namespace aggro
         using memory_resource = T*;
         using value_type = T;
 
-        memory_resource m_buffer = nullptr;
+        memory_resource m_buffer = nullptr; //Pointer to the beginning of the memory buffer.
 
+        //Returns a pointer to the memory resource.
         constexpr memory_resource resource() { return m_buffer; }
 
+        //Returns a pointer to the memory resource.
         constexpr const memory_resource resource() const { return m_buffer; }
 
+        //Sets the underlying pointer to a new memory buffer.
         constexpr void set_res(memory_resource other) { m_buffer = other; }
 
-        constexpr memory_resource allocate(size_type amount)
+        //Allocate a new memory buffer.
+        [[nodiscard]] constexpr memory_resource allocate(size_type amount)
         {
-            m_buffer = static_cast<memory_resource>(::operator new[](amount * sizeof(T)));
-
-            return m_buffer;
+            return static_cast<memory_resource>(::operator new[](amount * sizeof(T)));
         }
 
+        //Deallocate a memory resource, specifying the size of the buffer.
         constexpr void deallocate(memory_resource start, size_type size)
         {
             ::operator delete[](start, size * sizeof(T));
         }
         
+        //Construct an object at the specified location using placement new.
         template<typename... Args>
         constexpr void construct(memory_resource spot, Args&&... args)
         {
@@ -47,24 +51,30 @@ namespace aggro
         using memory_resource = T*;
         using value_type = typename T::value_type;
 
-        memory_resource m_head_node = nullptr;
+        memory_resource m_head_node = nullptr; //Pointer to the head node.
 
+        //Returns a pointer to the head node.
         constexpr memory_resource resource() { return m_head_node; }
 
+        //Returns a pointer to the head node.
         constexpr const memory_resource resource() const { return m_head_node; }
 
+        //Changes the node that is pointed to as the head.
         constexpr void set_res(memory_resource other) { m_head_node = other; }
 
+        //Allocates a new node and returns a pointer to it.
         [[nodiscard]] constexpr memory_resource allocate(size_type amount)
         {
             return static_cast<memory_resource>(::operator new[](amount * sizeof(T)));
         }
 
+        //Deallocates a node.
         constexpr void deallocate(memory_resource start, size_type size)
         {
             ::operator delete[](start, size * sizeof(T));
         }
         
+        //Constructs an object into the specified node using placement new.
         template<typename... Args>
         constexpr void construct(memory_resource spot, Args&&... args)
         {
