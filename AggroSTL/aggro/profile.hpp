c4114bab0ec3cc8befb4d0cdd69268c805c54fd4
Samuel Bridgham <sbridgham456@gmail.com>
2021-08-24 13:20:33 -0400

    df

---
+++ AggroSTL/aggro/profile.hpp
@@ -0,0 +1,95 @@
+#ifndef MEM_PROFILE_HPP
+#define MEM_PROFILE_HPP
+#include <iostream>
+#include <chrono>
+
+namespace aggro
+{
+    /*
+        This struct is used to keep track of memory allocations and deallocations in order to detect memory leaks.
+        All test functions in the test suite require you to pass in an object of type head_counter and execute it
+        via the macro MEM_CHECK.
+    */
+    struct heap_counter
+    {
+        inline static size_t mem_alloc = 0u;
+        inline static size_t mem_delete = 0u;
+
+        inline static void add(size_t bytes)
+        {
+            mem_alloc += bytes;
+        }
+
+        inline static void remove(size_t bytes)
+        {
+            mem_delete += bytes;
+        }
+
+        heap_counter() = default;
+        ~heap_counter()
+        {
+            std::cout << mem_alloc << " bytes of memory allocated.\n" << mem_delete << " bytes deallocated.\n\n";
+            mem_alloc = 0u;
+            mem_delete = 0u;
+        }
+    };
+
+    /*
+        This struct times a function. By comparing speed with the STL we can determine how well, or poorly, our code is optimized.
+    */
+    struct bench_timer
+    {
+        inline static std::chrono::high_resolution_clock::time_point time = std::chrono::high_resolution_clock::now();
+
+        bench_timer() = default;
+        ~bench_timer()
+        {
+            std::chrono::high_resolution_clock::time_point current = std::chrono::high_resolution_clock::now();
+            std::cout << "Function finished in " << std::chrono::duration_cast<std::chrono::microseconds>(current - time)
+            << " microseconds.\n";
+
+            time = current;
+        }
+    };
+
+} // namespace aggro
+
+#ifdef AGGRO_MEMORY_PROFILE
+/*
+    Override global new and delete operators.
+*/
+
+[[nodiscard]] inline void* operator new(size_t size)  //Global single object
+{
+    aggro::heap_counter::add(size);
+    return malloc(size);
+}
+
+[[nodiscard]] inline void* operator new[](size_t size)    //Global object array
+{
+    aggro::heap_counter::add(size);
+    return malloc(size);
+}
+
+inline void operator delete(void* ptr, size_t size) //Global single object delete
+{
+    aggro::heap_counter::remove(size);
+    free(ptr);
+}
+
+inline void operator delete[](void* ptr, size_t size)   //Global object array delete
+{
+    aggro::heap_counter::remove(size);
+    free(ptr);
+}
+
+#define MEM_CHECK(func) \
+std::cout << "Function name " << #func << ":\n";\
+func(aggro::bench_timer(), aggro::heap_counter());
+
+#else
+#define MEM_CHECK(func) 
+
+#endif //AGGRO_MEMORY_PROFILE
+
+#endif // MEM_PROFILE_HPP
