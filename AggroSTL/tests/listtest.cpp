62c0dc0ace550f1eb3bed66e5ce7afaa391f39dd
Samuel Bridgham <sbridgham456@gmail.com>
2021-08-24 13:28:13 -0400

    added bench_timer and std::lists to tests

--- AggroSTL/tests/listtest.cpp
+++ AggroSTL/tests/listtest.cpp
@@ -1,9 +1,11 @@
 #define AGGRO_MEMORY_PROFILE
-#include "mem_profile.hpp"
+#include "profile.hpp"
 #include "list.hpp"
 #include <string>
+#include <forward_list>
+#include <list>
 
-static void test_slist_with_pod([[maybe_unused]] aggro::heap_counter h)
+static void test_slist_with_pod([[maybe_unused]] aggro::bench_timer t, [[maybe_unused]] aggro::heap_counter h)
 {
     //plain data test
 
@@ -28,7 +30,7 @@ static void test_slist_with_pod([[maybe_unused]] aggro::heap_counter h)
     std::cout << nums << "\n";
 }
 
-static void test_dlist_with_strings([[maybe_unused]] aggro::heap_counter h)
+static void test_dlist_with_strings([[maybe_unused]] aggro::bench_timer t, [[maybe_unused]] aggro::heap_counter h)
 {
     // strings test
 
@@ -54,14 +56,33 @@ static void test_dlist_with_strings([[maybe_unused]] aggro::heap_counter h)
     std::cout << words << "\n";
 }
 
-static void test_list_from_empty([[maybe_unused]] aggro::heap_counter h)
+static void test_list_from_empty([[maybe_unused]] aggro::bench_timer t, [[maybe_unused]] aggro::heap_counter h)
 {
     aggro::slist<size_t> single;
     aggro::dlist<size_t> dub;
 
-    for(size_t i = 0u; i < 100u; ++i)
+    for(size_t i = 0u; i < 1000u; ++i)
     {
-        if(i % 2u == 0u)
+        if(i % 4u == 3u)
+        {
+            dub.pop_front();
+            single.erase_after(single.begin());
+        }
+        
+        dub.emplace_back(i);
+        single.emplace_front(i);
+    }
+
+}
+
+static void std_list_from_empty([[maybe_unused]] aggro::bench_timer t, [[maybe_unused]] aggro::heap_counter h)
+{
+    std::forward_list<size_t> single;
+    std::list<size_t> dub;
+
+    for(size_t i = 0u; i < 1000u; ++i)
+    {
+        if(i % 4u == 3u)
         {
             dub.pop_front();
             single.erase_after(single.begin());
@@ -80,5 +101,6 @@ int main()
     MEM_CHECK(test_slist_with_pod)
     MEM_CHECK(test_dlist_with_strings)
     MEM_CHECK(test_list_from_empty)
+    MEM_CHECK(std_list_from_empty)
     
 }
